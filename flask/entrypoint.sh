eb06a0884b0f37db05fb9e74126ef9e2c02aa8ce
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-03-21 10:07:13 -0300

    style: changing filenames

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 flask_app.py db init
-python3 flask_app.py db migrate
-python3 flask_app.py db upgrade
-python3 flask_app.py runserver
+python3 app.py db init
+python3 app.py db migrate
+python3 app.py db upgrade
+python3 app.py runserver
