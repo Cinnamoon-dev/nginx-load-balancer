0d4349b12ff05d015f4a3d37eae0af32661bacb3
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-04-01 10:57:06 -0300

    feat: making code more robust

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 app.py db init
-python3 app.py db migrate
-python3 app.py db upgrade
+python3 app.py db init &&
+python3 app.py db migrate &&
+python3 app.py db upgrade &&
 python3 app.py runserver
